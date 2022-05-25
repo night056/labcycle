2de5541bb4a8b289857bedc884848aa0c1d48b28
night056 <93523775+night056@users.noreply.github.com>
2022-05-25 15:25:41 +0530

    Create labcycle1.c

---
+++ labcycle1.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	struct node
+	{
+		int data;
+		struct node *next;
+	};
+	struct node *head=NULL, *pos=NULL, *tail=NULL;
+	int ch, e,i=0,j=1,a;
+	while(1)
+	{
+		printf("\n1.Insert\n2.Display\n3.delete\n4.Search for an element\n5.Exit");
+		printf("\nenter your choice:\n");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1:
+			{
+				printf("\nenter data:\n");
+				scanf("%d",&e);
+				if(head==NULL)
+				{
+					head=(struct node*)(malloc(sizeof(struct node)));
+					head->data=e;
+					pos=head;
+					tail=head;
+				}
+				else
+				{
+					tail->next=(struct node*)(malloc(sizeof(struct node)));
+					tail=tail->next;
+					tail->data=e;
+				}
+				i++;
+				break;
+			}
+			case 2:
+			{
+				pos=head;
+				printf("\nthe elements are:\n");
+				while(pos!=NULL)
+				{
+					printf("\t%d",pos->data);
+					pos=pos->next;
+				}
+				break;
+			}
+			case 3:
+			{
+				struct node *temp=head;
+				printf("\nEnter position you would like to delete(start from 0):\n");
+				scanf("%d",&e);
+				if(e==0)
+				{
+					head=head->next;
+					temp->next=NULL;
+					free(temp);
+				}
+				else
+				{
+					for(int i=0;i<e-1;i++)
+						temp=temp->next;
+					struct node *del=temp->next;
+					temp->next=temp->next->next;
+					del->next=NULL;
+					free(del);
+					}break;
+			}
+			case 4:
+			{
+				printf("\nenter an element which you would like to search for:\n");
+				scanf("%d",&a);
+				pos=head;
+				while(pos!=NULL)
+				{
+					if(pos->data==a)
+						printf("\nthe element you have entered is in position %d",j);
+					j++;
+					pos=pos->next;
+				}
+				break;
+			}
+			case 5:
+			{
+				printf("\ngoodbye :)");
+				exit(0);
+			}
+		}
+	}
+}
