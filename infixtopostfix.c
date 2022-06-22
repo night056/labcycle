8de1af0437f568b546ef4833a59b0761ac653c2a
night056 <93523775+night056@users.noreply.github.com>
2022-06-22 13:30:44 +0530

    Create infixtopostfix.c

---
+++ infixtopostfix.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define n 100
+int top;
+
+char infix[n],postfix[n];
+
+void push (char x)
+{
+	top++;
+	postfix[top]=x;
+}
+
+char pop()
+{
+	if(top== -1)
+	{
+		return 1;
+	}
+	else
+	{
+		return postfix[top--];
+	}
+}
+
+int priority(char x)
+{
+	if(x== '+' ||  x== '-')
+	{
+		return 1;
+	}
+	if (x== '*' || x== '/')
+	{
+		return 2;
+	}
+	return 0;
+}
+
+
+int main()
+{
+	char *e,x;
+	printf("Enter the expression in infix form:");
+	scanf("%s",infix);
+	printf("\n");
+	e=infix;
+	while (*e!= '\0')
+	{
+		if (isalnum(*e))
+		{
+			printf("%c",*e);
+		}
+		else if (*e == '(')
+		{
+			push(*e);
+		}
+		else if (*e == ')')
+		{
+			while (x != '(' )
+			{
+				printf("%c",pop());
+			}
+		}
+		else
+		{
+			while (priority(postfix[top]) >= priority (*e))
+			{
+				printf("%c",pop());
+			}
+			push(*e);
+		}
+		e++;
+	}
+	while(top != -1)
+	{
+		printf("%c",pop());
+	}
+	printf("\n");
+	return 0;
+}
